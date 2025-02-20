57644d74b7f0e822715b32bf5cf55b314e5ce72c
Marco Grecuccio <159437931+mgrecuccio@users.noreply.github.com>
2025-02-20 12:56:29 +0000

    Lesson 3 - activating authorized check

--- packages/form/auth/auth.py
+++ packages/form/auth/auth.py
@@ -7,7 +7,6 @@ def unauthorized(args):
   return check.decode("utf-8") != secret
   
 def auth(args):
-  print("Token:", args.get("token", "<none>"))
-  #if unauthorized(args):
-  #  return { "output": "you are not authenticated" }
+  if unauthorized(args):
+   return { "output": "you are not authenticated" }
   return { "output": "you are authenticated" }
