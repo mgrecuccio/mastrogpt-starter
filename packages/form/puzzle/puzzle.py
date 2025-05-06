4ac7ac65f6971c3e5067133053dbdc7ab54b3526
mgrecuccio <marco.grecuccio01@gmail.com>
2025-05-06 23:03:30 +0200

    Lesson 3: Forms and DDisplay

---
+++ packages/form/puzzle/puzzle.py
@@ -0,0 +1,85 @@
+import re, os, requests as req
+MODEL = "phi4:14b"
+
+FORM = [
+  {
+    "name": "queen",
+    "label": "Whit a queen",
+    "type": "checkbox"
+  },
+  {
+    "name": "rook",
+    "label": "With a rook",
+    "type": "checkbox"
+  },
+  {
+    "name": "bishop",
+    "label": "With a bishop",
+    "type": "checkbox"
+  },
+  {
+    "name": "knight",
+    "label": "With a knight",
+    "type": "checkbox"
+  }
+]
+
+def chat(args, inp):
+  host = args.get("OLLAMA_HOST", os.getenv("OLLAMA_HOST"))
+  auth = args.get("AUTH", os.getenv("AUTH"))
+  url = f"https://{auth}@{host}/api/generate"
+  msg = { "model": MODEL, "prompt": inp, "stream": False}
+  res = req.post(url, json=msg).json()
+  out = res.get("response", "error")
+  return  out
+ 
+def extract_fen(out):
+  pattern = r"([rnbqkpRNBQKP1-8]+\/){7}[rnbqkpRNBQKP1-8]+"
+  fen = None
+  m = re.search(pattern, out, re.MULTILINE)
+  if m:
+    fen = m.group(0)
+  return fen
+
+def puzzle(args):
+  out = "If you want to see a chess puzzle, type 'puzzle'. To display a fen position, type 'fen <fen string>'."
+  inp = args.get("input", "")
+  res = {}
+  
+  if type(inp) is dict and "form" in inp:
+    data = inp["form"]
+    
+    inp = "generate a chess puzzle in FEN format"
+
+    if data.get("rook"):
+      inp += " with a rook"
+    if data.get("queen"):
+      inp += " with a queen"
+    if data.get("bishop"):
+      inp += " with a bishop"
+    if data.get("knight"):
+      inp += " with a knight"
+
+    out = chat(args, inp)
+    fen = extract_fen(out)
+    if fen:
+       print(fen)
+       res['chess'] = fen
+    else:
+      out = "Bad FEN position."
+  elif inp == "puzzle":
+    res['form'] = FORM
+  elif inp.startswith("fen"):
+    fen = extract_fen(inp)
+    if fen:
+       out = "Here you go."
+       res['chess'] = fen
+  elif inp != "":
+    out = chat(args, inp)
+    fen = extract_fen(out)
+    print(out, fen)
+    if fen:
+      res['chess'] = fen
+
+  res["output"] = out
+  return res
